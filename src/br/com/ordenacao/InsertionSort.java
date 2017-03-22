033df189beceeee2bb9ebc69f5a855c92d0f1d4a
aluno <aluno@LAB202-17>
2017-03-22 20:12:27 -0300

    implementando o selectionSort

--- src/br/com/ordenacao/InsertionSort.java
+++ src/br/com/ordenacao/InsertionSort.java
@@ -10,5 +10,33 @@ package br.com.ordenacao;
  * @author Evandro
  */
 public class InsertionSort {
-    
+    public static void main(String[] args) {
+        int[] element = {39,14,52,65,7,1,5,6,4};
+        int chave;
+        int i;
+        
+        System.out.print("Vetor Desordenado element[j]= ");
+        //aqui mostra o vetor de element desordenado
+        for (int j = 0; j < element.length; j++)
+            System.out.printf("[%d],",element[j]);
+        
+        System.out.println("\n");
+        
+        //Aqui faz a Ordenação
+        for (i = 0; i < element.length; i++) {
+            chave = element[i];
+            for (int j = i-1; j >= 0 && chave < element[j]; j--) {
+                element[j+1] = element[j];
+                
+            }
+            element[i+1] = chave;
+        }
+        
+        System.out.print("Vetor Ordenado element[j]= ");
+        //Aqui mostro já Ordenado
+        for (int j = 0; j < element.length; j++)
+            System.out.printf("[%d],",element[j]);
+               
+        System.out.println("\n");
+    }
 }
--- src/br/com/ordenacao/SelectionSort.java
+++ src/br/com/ordenacao/SelectionSort.java
@@ -5,10 +5,46 @@
  */
 package br.com.ordenacao;
 
+import java.util.Random;
+
 /**
  *
  * @author Evandro
  */
 public class SelectionSort {
-    
+    public static void main(String[] args) {
+        int selecao[] = new int[10];
+        Random ale = new Random();
+        int aux;
+        int temp;
+//        boolean verifica = true;
+        //Aqui preenche o vetor de forma aleatoria
+        System.out.print("Vetor desOrdenado vetor[i] = ");
+        for (int i = 0; i < selecao.length; i++) {
+            selecao[i] = ale.nextInt(100);
+            System.out.printf("%d,",selecao[i]);
+        }
+        System.out.println("\n");
+        
+        //Aqui faz a Ordenação
+        for (int i = 0; i <= selecao.length-2; i++) {
+            aux = i;
+            for (int j = i+1; j <= selecao.length-1; j++) {
+                if (selecao[j] < selecao[aux]) {
+                    aux = j;
+                }
+                
+            }
+               temp = selecao[i];
+                selecao[i] = selecao[aux];
+                selecao[aux] = temp;
+        }
+        
+        System.out.print("Vetor Ordenado vetor[i]= ");
+        //Aqui mostra o vetor Ordenado
+        for (int i = 0; i < selecao.length; i++) {
+            System.out.printf("%d,",selecao[i]);
+        }
+        System.out.println();
+    }
 }
